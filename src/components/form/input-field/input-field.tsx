362a279f64fc009f3d237f5846ff6d49d6890895
Steven Vu <kvv190001@utdallas.edu>
2025-12-09 14:44:02 -0600

    Add output file to publish

--- src/components/form/input-field/input-field.tsx
+++ src/components/form/input-field/input-field.tsx
@@ -74,7 +74,7 @@ interface InputFieldProps extends Omit<ComponentProps<'input'>, 'children' | 'si
 }
 
 const ErrorIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4.5">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
     </svg>
 );
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./components/form/button/button";
+export * from "./components/form/checkbox/checkbox";
+export * from "./components/form/dropdown/dropdown";
+export * from "./components/form/input-field/input-field";
+export * from "./components/form/radio-group/radio-group";
